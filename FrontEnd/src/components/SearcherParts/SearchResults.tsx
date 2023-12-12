0e744ff5df8d296638e13e4145f35e5f94b4f001
“Jason-Uranta” <jason_uranta@brown.edu>
2023-12-11 22:14:20 -0500

    saving frontend

---
+++ FrontEnd/src/components/SearcherParts/SearchResults.tsx
@@ -0,0 +1,164 @@
+import '../../styles/main.css';
+
+
+interface SearchResultsProps{
+    results : String[];
+}
+
+export function SearchResults(props: SearchResultsProps){
+
+
+    return(
+        <div>
+            <br></br>
+            <br></br>
+            <br></br>
+        <table className="center" >
+            <tbody>
+                {/* ROW 1 */}
+                <tr>
+                <td className='scrollable-cell'>
+                <img
+                    // class="fit-picture"
+                src="..\..\images\grapefruit-slice.jpg"
+                alt="Grapefruit slice atop a pile of other slices" /> 
+                <div className='description-div'><p> START Verbose Filler Text 
+                    Verbose Filler Text Verbose Filler Text Verbose Filler Text 
+                    Verbose Filler Text Verbose Filler Text Verbose Filler Text 
+                    Verbose Filler Text Verbose Filler Text Verbose Filler Text 
+                    Verbose Filler Text Verbose Filler Text Verbose Filler Text 
+                    Verbose Filler Text Verbose Filler Text Verbose Filler Text 
+                    Verbose Filler Text Verbose Filler Text Verbose Filler Text 
+                    Verbose Filler Text Verbose Filler Text Verbose Filler Text 
+                    Verbose Filler Text Verbose Filler Text Verbose Filler Text 
+                    END</p></div>
+                </td>
+                <td className='scrollable-cell'>
+                <img
+                    // class="fit-picture"
+                src="..\..\images\grapefruit-slice.jpg"
+                alt="Grapefruit slice atop a pile of other slices" />
+                <div className='description-div'><p> START Verbose Filler Text 
+                    Verbose Filler Text Verbose Filler Text Verbose Filler Text 
+                    Verbose Filler Text Verbose Filler Text Verbose Filler Text 
+                    Verbose Filler Text Verbose Filler Text Verbose Filler Text 
+                    Verbose Filler Text Verbose Filler Text Verbose Filler Text 
+                    Verbose Filler Text Verbose Filler Text Verbose Filler Text 
+                    Verbose Filler Text Verbose Filler Text Verbose Filler Text 
+                    Verbose Filler Text Verbose Filler Text Verbose Filler Text 
+                    Verbose Filler Text Verbose Filler Text Verbose Filler Text 
+                    END</p></div> 
+                </td>
+                <td className='scrollable-cell'>
+                <img
+                    // class="fit-picture"
+                src="..\..\images\grapefruit-slice.jpg"
+                alt="Grapefruit slice atop a pile of other slices" /> 
+                <div className='description-div'><p> Filler Text</p></div> 
+                </td>
+                <td className='scrollable-cell'>
+                <img
+                    // class="fit-picture"
+                src="..\..\images\grapefruit-slice.jpg"
+                alt="Grapefruit slice atop a pile of other slices" /> 
+                <div className='description-div'><p> Filler Text</p></div> 
+                </td>
+                </tr>
+                <br></br>
+                <br></br>
+                <br></br>
+                {/* ROW 2 */}
+                <tr>
+                <td className='scrollable-cell'>
+                <img
+                    // class="fit-picture"
+                src="..\..\images\grapefruit-slice.jpg"
+                alt="Grapefruit slice atop a pile of other slices" /> 
+                <div className='description-div'><p> START Verbose Filler Text 
+                    Verbose Filler Text Verbose Filler Text Verbose Filler Text 
+                    Verbose Filler Text Verbose Filler Text Verbose Filler Text 
+                    Verbose Filler Text Verbose Filler Text Verbose Filler Text 
+                    Verbose Filler Text Verbose Filler Text Verbose Filler Text 
+                    Verbose Filler Text Verbose Filler Text Verbose Filler Text 
+                    Verbose Filler Text Verbose Filler Text Verbose Filler Text 
+                    Verbose Filler Text Verbose Filler Text Verbose Filler Text 
+                    Verbose Filler Text Verbose Filler Text Verbose Filler Text 
+                    END</p></div>
+                </td>
+                <td className='scrollable-cell'>
+                <img
+                    // class="fit-picture"
+                src="..\..\images\grapefruit-slice.jpg"
+                alt="Grapefruit slice atop a pile of other slices" />
+                <div className='description-div'><p> Filler Text</p></div> 
+                </td>
+                <td className='scrollable-cell'>
+                <img
+                    // class="fit-picture"
+                src="..\..\images\grapefruit-slice.jpg"
+                alt="Grapefruit slice atop a pile of other slices" /> 
+                <div className='description-div'><p> Filler Text</p></div> 
+                </td>
+                <td className='scrollable-cell'>
+                <img
+                    // class="fit-picture"
+                src="..\..\images\grapefruit-slice.jpg"
+                alt="Grapefruit slice atop a pile of other slices" /> 
+                <div className='description-div'><p> Filler Text</p></div> 
+                </td>
+                </tr>
+                <br></br>
+                <br></br>
+                <br></br>
+                {/* ROW 3 */}
+                <tr>
+                <td className='scrollable-cell'>
+                <img
+                    // class="fit-picture"
+                src="..\..\images\grapefruit-slice.jpg"
+                alt="Grapefruit slice atop a pile of other slices" /> 
+                <div className='description-div'><p> START Verbose Filler Text 
+                    Verbose Filler Text Verbose Filler Text Verbose Filler Text 
+                    Verbose Filler Text Verbose Filler Text Verbose Filler Text 
+                    Verbose Filler Text Verbose Filler Text Verbose Filler Text 
+                    Verbose Filler Text Verbose Filler Text Verbose Filler Text 
+                    Verbose Filler Text Verbose Filler Text Verbose Filler Text 
+                    Verbose Filler Text Verbose Filler Text Verbose Filler Text 
+                    Verbose Filler Text Verbose Filler Text Verbose Filler Text 
+                    Verbose Filler Text Verbose Filler Text Verbose Filler Text 
+                    END</p></div>
+                </td>
+                <td className='scrollable-cell'>
+                <img
+                    // class="fit-picture"
+                src="..\..\images\grapefruit-slice.jpg"
+                alt="Grapefruit slice atop a pile of other slices" />
+                <div className='description-div'><p> Filler Text</p></div> 
+                </td>
+                <td className='scrollable-cell'>
+                <img
+                    // class="fit-picture"
+                src="..\..\images\grapefruit-slice.jpg"
+                alt="Grapefruit slice atop a pile of other slices" /> 
+                <div className='description-div'><p> Filler Text</p></div> 
+                </td>
+                <td className='scrollable-cell'>
+                <img
+                    // class="fit-picture"
+                src="..\..\images\grapefruit-slice.jpg"
+                alt="Grapefruit slice atop a pile of other slices" /> 
+                <div className='description-div'><p> Filler Text</p></div> 
+                </td>
+                </tr>
+            </tbody>
+        </table>
+
+        
+        {/* <img
+            // class="fit-picture"
+            src="..\..\images\grapefruit-slice.jpg"
+            alt="Grapefruit slice atop a pile of other slices" />  */}
+        </div>
+    )
+
+}
