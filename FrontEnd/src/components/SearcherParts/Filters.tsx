cd08378a0358337f5616ed5abc075c50148c88ee
ryanlyang <ryan_l_yang@brown.edu>
2023-12-12 21:34:26 -0500

    added filter and hover

---
+++ FrontEnd/src/components/SearcherParts/Filters.tsx
@@ -0,0 +1,37 @@
+import '../../styles/filters.css';
+import { Dispatch, HtmlHTMLAttributes, SetStateAction, useState} from 'react';
+
+export function Filters(){
+    const [isChecked, setIsChecked] = useState(true);
+    return(
+        <div>
+            <p></p>
+            <div className = "dual-container">
+                <div className="input-container">
+                    <div>Release Date:</div>
+                    <input type='month'></input>
+                    <div>  to  </div>
+                    <input type='month'></input>
+                    <div className='apply-filter'> Apply Filter: </div>
+                    <input type='checkbox' 
+                    checked={isChecked}
+                    onChange={() => setIsChecked(!isChecked)} />
+                </div>
+
+                <div className="input-container">
+                    <div>IMDB Rating:</div>
+                    <input type='number'
+                    placeholder='e.g. 1.0'></input>
+                    <div>  to  </div>
+                    <input type='number'
+                    placeholder='e.g. 10.0'></input>
+                    <div className='apply-filter'> Apply Filter: </div>
+                    <input type='checkbox' 
+                    checked={isChecked}
+                    onChange={() => setIsChecked(!isChecked)} />
+                </div>
+            </div>
+            
+        </div>
+    )
+}
