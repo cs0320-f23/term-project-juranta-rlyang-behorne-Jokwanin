8330abea05b2cb5226908b5f5413dd7664c742e3
Josh2026 <joshua_okwaning@brown.edu>
2023-12-12 20:13:22 -0500

    merging with database

--- backend/src/main/java/edu/brown/cs/student/main/Handler/movieHandler.java
+++ backend/src/main/java/edu/brown/cs/student/main/Handler/movieHandler.java
@@ -1,44 +1,44 @@
-package edu.brown.cs.student.main.Handler;
-
-import com.squareup.moshi.JsonAdapter;
-import com.squareup.moshi.Moshi;
-import com.squareup.moshi.Types;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import spark.Request;
-import spark.Response;
-import spark.Route;
-
-public class movieHandler {
-  @Override
-  public Object handle(Request request, Response response) {
-    List<List<String>> parsedData = this.parsedData.getData();
-    if (parsedData == null) {
-      return this.serialize("error_datasource", new ArrayList<>());
-    }
-    return this.serialize("success", parsedData);
-  }
-
-  /**
-   * A method that encapsulates the serialization of a JSON object. This method is used to serialize
-   * the results from searching the CSV into a JSON object for the user to see.
-   *
-   * @param result - Whether the execution of this request was done successfully or not
-   * @param data - The parsed CSV data
-   * @return - A string found by serializing a Map with the above values
-   */
-  private String serialize(String result, List<List<String>> data) {
-    Moshi moshi = new Moshi.Builder().build();
-    Type mapStringObject = Types.newParameterizedType(Map.class, String.class, Object.class);
-    JsonAdapter<Map<String, Object>> adapter = moshi.adapter(mapStringObject);
-    Map<String, Object> responseMap = new HashMap<>();
-
-    responseMap.put("result", result);
-    responseMap.put("data", data);
-    return adapter.toJson(responseMap);
-  }
-
-}
+//package edu.brown.cs.student.main.Handler;
+//
+//import com.squareup.moshi.JsonAdapter;
+//import com.squareup.moshi.Moshi;
+//import com.squareup.moshi.Types;
+//import java.lang.reflect.Type;
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//import spark.Request;
+//import spark.Response;
+//import spark.Route;
+//
+//public class movieHandler {
+//  @Override
+//  public Object handle(Request request, Response response) {
+//    List<List<String>> parsedData = this.parsedData.getData();
+//    if (parsedData == null) {
+//      return this.serialize("error_datasource", new ArrayList<>());
+//    }
+//    return this.serialize("success", parsedData);
+//  }
+//
+//  /**
+//   * A method that encapsulates the serialization of a JSON object. This method is used to serialize
+//   * the results from searching the CSV into a JSON object for the user to see.
+//   *
+//   * @param result - Whether the execution of this request was done successfully or not
+//   * @param data - The parsed CSV data
+//   * @return - A string found by serializing a Map with the above values
+//   */
+//  private String serialize(String result, List<List<String>> data) {
+//    Moshi moshi = new Moshi.Builder().build();
+//    Type mapStringObject = Types.newParameterizedType(Map.class, String.class, Object.class);
+//    JsonAdapter<Map<String, Object>> adapter = moshi.adapter(mapStringObject);
+//    Map<String, Object> responseMap = new HashMap<>();
+//
+//    responseMap.put("result", result);
+//    responseMap.put("data", data);
+//    return adapter.toJson(responseMap);
+//  }
+//
+//}
--- backend/src/main/java/edu/brown/cs/student/main/setup/Setup.java
+++ backend/src/main/java/edu/brown/cs/student/main/setup/Setup.java
@@ -1,67 +1,67 @@
-package edu.brown.cs.student.main.setup;
-
-import edu.brown.cs.student.main.csv.FactoryFailureException;
-import edu.brown.cs.student.main.csv.creators.CreateArrayList;
-import edu.brown.cs.student.main.csv.creators.CreatorFromRow;
-import edu.brown.cs.student.main.csv.parser.CsvParser;
-import edu.brown.cs.student.main.csv.searcher.Search;
-
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-
-public class Setup {
-    public HashMap<String, HashMap<String, Object>> setup() throws IOException, FactoryFailureException {
-        FileReader fileReader = new FileReader("backend/data/ImdbTitleBasics.csv");
-        Search filterMovie = new Search(fileReader, "movie", "2", true, false);
-        ArrayList<ArrayList<String>> movieList = filterMovie.beginSearch();
-        CreatorFromRow<ArrayList<String>> creatorFromRow = new CreateArrayList();
-        fileReader = new FileReader("backend/data/ImdbName.csv");
-        CsvParser<ArrayList<String>> parsedName = new CsvParser<>(fileReader, creatorFromRow);
-        ArrayList<ArrayList<String>> people = parsedName.parse();
-        HashMap<String, String> nameMap = new HashMap<>();
-        for (ArrayList<String> person: people) {
-            nameMap.put(person.get(0), person.get(1));
-        }
-        fileReader = new FileReader("backend/data/ImdbTitleRatings.csv");
-        CsvParser<ArrayList<String>> parsedRatings = new CsvParser<>(fileReader, creatorFromRow);
-        ArrayList<ArrayList<String>> ratings = parsedRatings.parse();
-        HashMap<String, String> ratingsMap = new HashMap<>();
-        for (ArrayList<String> rating: ratings) {
-            ratingsMap.put(rating.get(0), rating.get(1));
-        }
-        fileReader = new FileReader("backend/data/ImdbTitleCrew.csv");
-        CsvParser<ArrayList<String>> parsedCrew = new CsvParser<>(fileReader, creatorFromRow);
-        ArrayList<ArrayList<String>> crew = parsedCrew.parse();
-        HashMap<String, String[]> directors = new HashMap<>();
-        for (ArrayList<String> director: crew) {
-            String[] list = director.get(1).split(",");
-            for (int i = 0; i < list.length; i++) {
-                list[i] = nameMap.get(list[i]);
-            }
-            directors.put(director.get(0), list);
-        }
-        HashMap<String, String[]> writers = new HashMap<>();
-        for (ArrayList<String> writer: crew) {
-            String[] list = writer.get(1).split(",");
-            for (int i = 0; i < list.length; i++) {
-                list[i] = nameMap.get(list[i]);
-            }
-            writers.put(writer.get(0), list);
-        }
-        HashMap<String, HashMap<String, Object>> movieDatabase = new HashMap<>();
-        for (ArrayList<String> movie:movieList) {
-            HashMap<String, Object> movieData = new HashMap<>();
-            movieData.put("id", movie.get(0));
-            movieData.put("title", movie.get(2));
-            movieData.put("year", movie.get(5));
-            movieData.put("genre", movie.get(8));
-            movieData.put("ratings", ratingsMap.get(movie.get(0)));
-            movieData.put("directors", directors.get(movie.get(0)));
-            movieData.put("writers", writers.get(movie.get(0)));
-            movieDatabase.put(movie.get(2), movieData);
-        }
-        return movieDatabase;
-    }
+//package edu.brown.cs.student.main.setup;
+//
+//import edu.brown.cs.student.main.csv.FactoryFailureException;
+//import edu.brown.cs.student.main.csv.creators.CreateArrayList;
+//import edu.brown.cs.student.main.csv.creators.CreatorFromRow;
+//import edu.brown.cs.student.main.csv.parser.CsvParser;
+//import edu.brown.cs.student.main.csv.searcher.Search;
+//
+//import java.io.FileReader;
+//import java.io.IOException;
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//
+//public class Setup {
+//    public HashMap<String, HashMap<String, Object>> setup() throws IOException, FactoryFailureException {
+//        FileReader fileReader = new FileReader("backend/data/ImdbTitleBasics.csv");
+//        Search filterMovie = new Search(fileReader, "movie", "2", true, false);
+//        ArrayList<ArrayList<String>> movieList = filterMovie.beginSearch();
+//        CreatorFromRow<ArrayList<String>> creatorFromRow = new CreateArrayList();
+//        fileReader = new FileReader("backend/data/ImdbName.csv");
+//        CsvParser<ArrayList<String>> parsedName = new CsvParser<>(fileReader, creatorFromRow);
+//        ArrayList<ArrayList<String>> people = parsedName.parse();
+//        HashMap<String, String> nameMap = new HashMap<>();
+//        for (ArrayList<String> person: people) {
+//            nameMap.put(person.get(0), person.get(1));
+//        }
+//        fileReader = new FileReader("backend/data/ImdbTitleRatings.csv");
+//        CsvParser<ArrayList<String>> parsedRatings = new CsvParser<>(fileReader, creatorFromRow);
+//        ArrayList<ArrayList<String>> ratings = parsedRatings.parse();
+//        HashMap<String, String> ratingsMap = new HashMap<>();
+//        for (ArrayList<String> rating: ratings) {
+//            ratingsMap.put(rating.get(0), rating.get(1));
+//        }
+//        fileReader = new FileReader("backend/data/ImdbTitleCrew.csv");
+//        CsvParser<ArrayList<String>> parsedCrew = new CsvParser<>(fileReader, creatorFromRow);
+//        ArrayList<ArrayList<String>> crew = parsedCrew.parse();
+//        HashMap<String, String[]> directors = new HashMap<>();
+//        for (ArrayList<String> director: crew) {
+//            String[] list = director.get(1).split(",");
+//            for (int i = 0; i < list.length; i++) {
+//                list[i] = nameMap.get(list[i]);
+//            }
+//            directors.put(director.get(0), list);
+//        }
+//        HashMap<String, String[]> writers = new HashMap<>();
+//        for (ArrayList<String> writer: crew) {
+//            String[] list = writer.get(1).split(",");
+//            for (int i = 0; i < list.length; i++) {
+//                list[i] = nameMap.get(list[i]);
+//            }
+//            writers.put(writer.get(0), list);
+//        }
+//        HashMap<String, HashMap<String, Object>> movieDatabase = new HashMap<>();
+//        for (ArrayList<String> movie:movieList) {
+//            HashMap<String, Object> movieData = new HashMap<>();
+//            movieData.put("id", movie.get(0));
+//            movieData.put("title", movie.get(2));
+//            movieData.put("year", movie.get(5));
+//            movieData.put("genre", movie.get(8));
+//            movieData.put("ratings", ratingsMap.get(movie.get(0)));
+//            movieData.put("directors", directors.get(movie.get(0)));
+//            movieData.put("writers", writers.get(movie.get(0)));
+//            movieDatabase.put(movie.get(2), movieData);
+//        }
+//        return movieDatabase;
+//    }
 }
