16c2474b7a7d0d8de0639fd5a1a26be1a6bbaa3b
Josh2026 <joshua_okwaning@brown.edu>
2023-12-21 11:32:12 -0500

    testing for setup

--- backend/src/main/java/edu/brown/cs/student/main/setup/Setup.java
+++ backend/src/main/java/edu/brown/cs/student/main/setup/Setup.java
@@ -24,12 +24,12 @@ public class Setup {
   private HashMap<String, String> writers;
 
     public Setup() throws IOException, FactoryFailureException {
-        FileReader fileReader = new FileReader("data/ImdbTitleBasics.csv");
+        FileReader fileReader = new FileReader("/backend/data/ImdbTitleBasics.csv");
         Search filterMovie = new Search(fileReader, "movie", "2", true, false);
         this.movieList = filterMovie.beginSearch();
 
         CreatorFromRow<ArrayList<String>> creatorFromRow = new CreateArrayList();
-        fileReader = new FileReader("data/ImdbName.csv");
+        fileReader = new FileReader("backend/data/ImdbName.csv");
         CsvParser<ArrayList<String>> parsedName = new CsvParser<>(fileReader, creatorFromRow);
         ArrayList<ArrayList<String>> people = parsedName.parse();
         this.nameMap = new HashMap<>();
@@ -37,7 +37,7 @@ public class Setup {
             this.nameMap.put(person.get(0), person.get(1));
         }
 
-        fileReader = new FileReader("data/ImdbTitleCrew.csv");
+        fileReader = new FileReader("backend/data/ImdbTitleCrew.csv");
         CsvParser<ArrayList<String>> parsedCrew = new CsvParser<>(fileReader, creatorFromRow);
         ArrayList<ArrayList<String>> crew = parsedCrew.parse();
         this.directors = new HashMap<>();
@@ -51,7 +51,7 @@ public class Setup {
         }
     }
     public HashMap<String, HashMap<String, String>> setup() throws IOException, FactoryFailureException {
-        FileReader fileReader = new FileReader("data/ImdbTitleRatings.csv");
+        FileReader fileReader = new FileReader("backend/data/ImdbTitleRatings.csv");
         CreatorFromRow<ArrayList<String>> creatorFromRow = new CreateArrayList();
         CsvParser<ArrayList<String>> parsedRatings = new CsvParser<>(fileReader, creatorFromRow);
         ArrayList<ArrayList<String>> ratings = parsedRatings.parse();
--- backend/src/test/java/edu/brown/cs/student/main/datasource/TestSetup.java
+++ backend/src/test/java/edu/brown/cs/student/main/datasource/TestSetup.java
@@ -1,9 +1,12 @@
 package edu.brown.cs.student.main.datasource;
 
 import edu.brown.cs.student.main.csv.FactoryFailureException;
+import edu.brown.cs.student.main.csv.searcher.Search;
 import edu.brown.cs.student.main.ordering.Order;
 import edu.brown.cs.student.main.setup.Filter;
 import edu.brown.cs.student.main.setup.Setup;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import org.junit.Before;
 import org.junit.Test;
 import org.testng.Assert;
@@ -37,14 +40,23 @@ public class TestSetup {
     System.out.println(database.get("blade runner"));
   }
 
-    @org.junit.Test
+  /**
+   * test name: testFilterGenre()
+   * - this test checks just checks how the initialization of our filtering on movie looks like
+    */
+  @org.junit.Test
     public void testFilterGenre() {
         Filter filter = new Filter(this.database, this.genreDatabase, this.peopleDatabase);
         HashMap<String, HashMap<String, String>> filteredList = filter.getFilteredList("blade runner");
         System.out.println(filteredList);
     }
 
-    @Test
+  /**
+   * test name: testOrder:
+   * - this test checks the order functionality for the ranking of our movies when given a specific
+   * movie
+   */
+  @org.junit.Test
     public void testOrder() {
         Filter filter = new Filter(this.database, this.genreDatabase, this.peopleDatabase);
         HashMap<String, HashMap<String, String>> filteredList = filter.getFilteredList("blade runner");
@@ -52,4 +64,91 @@ public class TestSetup {
         ArrayList<HashMap<String, String>> orderedList = order.order(filteredList, this.database.get("blade runner"));
         System.out.println(orderedList);
     }
+
+
+  /**
+   * testGenreDatabase()
+   * - this test makes sure that movies with dissimilar genres are not grouped to together
+   */
+  @org.junit.Test
+  public void testGenreDatabase() throws IOException, FactoryFailureException {
+    Setup setup = new Setup();
+    // a movie whose genre is Drama
+    String dramaMovie = "My Official Wife";
+    System.out.println(setup.setupGenre().keySet());
+    Assert.assertTrue(setup.setupGenre().get("drama").contains(dramaMovie));
+
+  }
+
+  /**
+   * test name : testGenreDataBase2()
+   * - checks that if a movie has multiple genres, the genre database would have the
+   * movie as part of the values list for all genres it is applicable to
+   *
+   * @throws IOException
+   * @throws FactoryFailureException
+   */
+  @org.junit.Test
+  public void testGenreDatabase2() throws IOException, FactoryFailureException{
+    Setup setup = new Setup();
+    // genres that movie is applicable to: "Mystery, Romance, Short"
+    String movieName = "The Mysterious Lodger";
+    Assert.assertTrue(setup.setupGenre().get("mystery").contains(movieName));
+    Assert.assertTrue(setup.setupGenre().get("romance").contains(movieName));
+    Assert.assertTrue(setup.setupGenre().get("short").contains(movieName));
+  }
+
+  /**
+   * test name: testSetupPeopleDB()
+   * - randomly chose a director and checked if their movies were represented in the
+   * peopledatabase
+   * @throws IOException
+   * @throws FactoryFailureException
+   */
+  @org.junit.Test
+  public void testSetupPeopleDB() throws IOException, FactoryFailureException {
+    Assert.assertTrue(peopleDatabase.get("nm0000631").contains("blade runner"));
+    Assert.assertTrue(peopleDatabase.get("nm0000631").contains("conquest of paradise"));
+    Assert.assertTrue(peopleDatabase.get("nm0000631").contains("black rain"));
+
+    // checking if we have a director with all the movies that they have directed
+    // ridley scott
+    System.out.println(peopleDatabase.get("nm0000631"));
+  }
+
+  /**
+   * test name: testSetupPeopleDB2()
+   * - checks if our database for directors accounts for all directors, writers in csv data
+   * @throws IOException
+   * @throws FactoryFailureException
+   */
+  @org.junit.Test
+  public void testSetupPeopleDB2() throws IOException, FactoryFailureException{
+    Assert.assertEquals(this.peopleDatabase.keySet().size(), 139453);
+  }
+
+  /**
+   * method name: testGenreCount
+   * - checks if all the genres represented in the csv are accounted for by the
+   * genredatabase
+   */
+  @org.junit.Test
+  public void testGenreCount(){
+    // https://help.imdb.com/article/contribution/titles/genres/GZDRMS6R742JRGAG#
+    /* link above represents all 28 genres, and the reason why our program accounts for
+    * 29 is because we have key represented as "\n" which represents movies that did not have
+    * an indicated genre*/
+    Assert.assertEquals(this.genreDatabase.keySet().size(),  29);
+  }
+
+
+
+
+
+
+
+
+
+
+
 }
